e550df998e65de3eed0db271d277dd6c4a258af1
gnsals0904 <gnsals0904@naver.com>
2024-05-13 01:05:55 +0900

    [chore] 카카오맵에 검색한 내용에 따라 마커 추가 기능

--- frontend/src/views/MapView.vue
+++ frontend/src/views/MapView.vue
@@ -1,18 +1,85 @@
 <script setup>
 import { KakaoMap, KakaoMapMarker } from "vue3-kakao-maps";
-const coordinate = {
-  lat: 37.566826,
-  lng: 126.9786567,
+import { onMounted, ref, computed, reactive } from "vue";
+import { useRoute } from "vue-router";
+import axios from "axios";
+
+const route = useRoute();
+const tripData = ref([]);
+
+onMounted(async () => {
+  const { sidoCode, gugunCode, contentCode } = route.query;
+
+  if (sidoCode && gugunCode && contentCode) {
+    try {
+      const response = await axios.get(
+        "http://localhost/api/trip/listTripWithOutTitle",
+        {
+          params: {
+            sidoCode: parseInt(sidoCode),
+            gugunCode: parseInt(gugunCode),
+            contentCode: parseInt(contentCode),
+          },
+        }
+      );
+      tripData.value = response.data.map((item) => ({
+        ...item,
+        infoVisible: false,
+      }));
+      console.log("Trip Data:", tripData.value);
+    } catch (error) {
+      console.error("Error fetching trip data:", error);
+      alert("여행 정보를 가져오는 데 실패했습니다.");
+    }
+  } else {
+    alert("필요한 매개변수가 URL에 포함되지 않았습니다.");
+  }
+});
+
+// 기본 좌표를 서울 시청으로 설정하되, tripData에 값이 있으면 첫 번째 데이터의 좌표를 사용
+const defaultCoordinate = computed(() => {
+  if (tripData.value.length > 0) {
+    return {
+      lat: tripData.value[0].latitude,
+      lng: tripData.value[0].longitude,
+    };
+  }
+  return {
+    lat: 37.566826,
+    lng: 126.9786567,
+  };
+});
+
+const toggleInfoWindow = (item) => {
+  item.infoVisible = !item.infoVisible;
 };
 </script>
 
 <template>
   <div>
-    <KakaoMap :lat="coordinate.lat" :lng="coordinate.lng" :draggable="true">
+    <KakaoMap
+      :lat="defaultCoordinate.lat"
+      :lng="defaultCoordinate.lng"
+      :draggable="true"
+    >
       <KakaoMapMarker
-        :lat="coordinate.lat"
-        :lng="coordinate.lng"
-      ></KakaoMapMarker>
+        v-for="(item, index) in tripData"
+        :key="index"
+        :lat="item.latitude"
+        :lng="item.longitude"
+        :clickable="true"
+        @onClickKakaoMapMarker="toggleInfoWindow(item)"
+        :infoWindow="{
+          content: `<div style='width: 200px;'><h4>${
+            item.title
+          }</h4><img src='${item.img1}' alt='${
+            item.title
+          }' style='width: 100%;'><button onClickKakaoMapMarker='toggleInfoWindow(${JSON.stringify(
+            item
+          )})'>Close</button></div>`,
+          visible: item.infoVisible,
+        }"
+      />
     </KakaoMap>
   </div>
 </template>
