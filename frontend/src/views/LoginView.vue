af97489526e8c5c81e1821763b22f8cd158503f7
gnsals0904 <gnsals0904@naver.com>
2024-05-13 17:52:17 +0900

    [feat] 로그인 화면 디자인 변경 및 이미지 api 추가

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,91 +1,43 @@
 <script setup>
-import { ref } from 'vue';
-import router from '@/router';
-import client from '@/api/client';
-import { useAuthStore } from '@/stores/auth';
+import { ref } from "vue";
+import router from "@/router";
+import client from "@/api/client";
+import { useAuthStore } from "@/stores/auth";
 
 const authStore = useAuthStore();
-const username = ref('');
-const password = ref('');
+const username = ref("");
+const password = ref("");
 
 const handleLogin = async () => {
-  console.log('Logging in with:', username.value, password.value);
+  console.log("Logging in with:", username.value, password.value);
   try {
-    const response = await client.post('/member/login', {
+    const response = await client.post("/member/login", {
       userId: username.value,
       userPassword: password.value,
     });
 
     if (response.status !== 200) {
-      throw new Error('로그인 실패');
+      throw new Error("로그인 실패");
     }
 
-    sessionStorage.setItem('memberDto', JSON.stringify(response.data));
+    sessionStorage.setItem("memberDto", JSON.stringify(response.data));
     console.log(response.data);
     authStore.login(response.data);
-    alert('로그인 성공!');
-    router.push({ name: 'Landing' });
+    alert("로그인 성공!");
+    router.push({ name: "Landing" });
   } catch (error) {
-    console.error('로그인 실패:', error);
-    alert('로그인 실패: ' + error.message);
+    console.error("로그인 실패:", error);
+    alert("로그인 실패: " + error.message);
   }
 };
 </script>
 
 <template>
-  <div
-    class="min-h-screen flex flex-col items-center justify-center bg-gray-100"
-  >
-    <h1 class="text-xl font-bold mb-8">LoginView</h1>
-    <form class="w-full max-w-xs" @submit.prevent="handleLogin">
-      <div class="mb-4">
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2"
-          for="username"
-        >
-          Username
-        </label>
-        <input
-          v-model="username"
-          id="username"
-          type="text"
-          placeholder="Username"
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          required
-        />
-      </div>
-      <div class="mb-6">
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2"
-          for="password"
-        >
-          Password
-        </label>
-        <input
-          v-model="password"
-          id="password"
-          type="password"
-          placeholder="******************"
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-          required
-        />
-      </div>
-      <div class="flex items-center justify-between">
-        <button
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="submit"
-        >
-          Sign In
-        </button>
-      </div>
-    </form>
-  </div>
-
   <!--new form-->
   <div class="h-screen flex">
     <!-- 이미지 섹션 -->
     <div
-      class="hidden lg:flex w-full lg:w-1/2 justify-around items-center bg-blue-600 p-10 space-x-5"
+      class="hidden lg:flex w-full lg:w-1/2 justify-around items-center login_img_section"
     >
       <div class="text-center space-y-6">
         <h1 class="text-white text-4xl font-bold">Simple App</h1>
@@ -101,7 +53,7 @@ const handleLogin = async () => {
     <!-- 로그인 폼 섹션 -->
     <div class="flex w-full lg:w-1/2 justify-center items-center bg-white">
       <form
-        @submit="handleLogin"
+        @submit.prevent="handleLogin"
         class="bg-white rounded-lg shadow-xl p-5 w-full max-w-md"
       >
         <h1 class="text-gray-800 font-bold text-2xl mb-1">Hello Again!</h1>
@@ -144,7 +96,6 @@ const handleLogin = async () => {
 <style>
 .login_img_section {
   background: linear-gradient(rgba(2, 2, 2, 0.7), rgba(0, 0, 0, 0.7)),
-    url(https://images.unsplash.com/photo-1650825556125-060e52d40bd0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)
-      center center;
+    url(https://source.unsplash.com/1600x900/?ocean) center center;
 }
 </style>
