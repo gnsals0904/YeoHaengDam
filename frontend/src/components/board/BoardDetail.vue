ace9ecfd8eea656cf701f3595d07c84c7c46ca3b
gnsals0904 <gnsals0904@naver.com>
2024-05-19 17:37:58 +0900

    [feat] board comment 컴포넌트로 분리

--- frontend/src/components/board/BoardDetail.vue
+++ frontend/src/components/board/BoardDetail.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { defineProps, defineEmits } from "vue";
+import BoardComment from "@/components/board/BoardComment.vue";
 
 const props = defineProps({
   item: {
@@ -17,6 +18,22 @@ const emit = defineEmits(["close"]);
 const closeModal = () => {
   emit("close");
 };
+
+// Sample comments data
+const comments = [
+  {
+    author: "Joshua",
+    text: "Good post",
+    image:
+      "https://images.pexels.com/photos/1450082/pexels-photo-1450082.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+  },
+  {
+    author: "Kesha",
+    text: "This is amazing",
+    image:
+      "https://images.pexels.com/photos/3861456/pexels-photo-3861456.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+  },
+];
 </script>
 
 <template>
@@ -145,7 +162,7 @@ const closeModal = () => {
           </button>
         </div>
       </div>
-      <!-- 댓글 부분 -->
+      <!-- 현재 접속한 사용자 부분 -->
       <div class="col-span-1 rounded-lg relative pl-4 bg-white w-full flex-1">
         <header class="border-b border-grey-400">
           <a
@@ -160,90 +177,14 @@ const closeModal = () => {
             <p class="block ml-2 font-bold">Paul</p>
           </a>
         </header>
+        <!-- 댓글 -->
         <div>
-          <div class="pt-1">
-            <div class="text-sm mb-2 flex flex-start items-center">
-              <div>
-                <a
-                  href="#"
-                  class="cursor-pointer flex items-center text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out"
-                >
-                  <img
-                    class="h-8 w-8 rounded-full object-cover"
-                    src="https://images.pexels.com/photos/1450082/pexels-photo-1450082.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-                    alt="user"
-                  />
-                </a>
-              </div>
-              <p class="font-bold ml-2">
-                <a class="cursor-pointer">Joshua:</a>
-                <span class="text-gray-700 font-medium ml-1"> Good post </span>
-              </p>
-            </div>
-          </div>
-          <div class="text-sm mb-2 flex flex-start items-center">
-            <div>
-              <a
-                href="#"
-                class="cursor-pointer flex items-center text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out"
-              >
-                <img
-                  class="h-8 w-8 rounded-full object-cover"
-                  src="https://images.pexels.com/photos/3861456/pexels-photo-3861456.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-                  alt="user"
-                />
-              </a>
-            </div>
-            <p class="font-bold ml-2">
-              <a class="cursor-pointer">Kesha:</a>
-              <span class="text-gray-700 font-medium ml-1">
-                This is amazing
-              </span>
-            </p>
+          <div v-for="comment in comments" :key="comment.author">
+            <BoardComment :comment="comment" />
           </div>
         </div>
+        <!-- 글 작성 부분 -->
         <div class="absolute bottom-0 left-0 right-0 pl-4">
-          <div class="pt-4">
-            <div class="mb-2">
-              <div class="flex items-center">
-                <span class="mr-3 inline-flex items-center cursor-pointer">
-                  <svg
-                    class="fill-heart text-gray-700 inline-block h-7 w-7 heart"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                    />
-                  </svg>
-                </span>
-                <span class="mr-3 inline-flex items-center cursor-pointer">
-                  <svg
-                    class="text-gray-700 inline-block h-7 w-7"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                    />
-                  </svg>
-                </span>
-              </div>
-              <span class="text-gray-600 text-sm font-bold">2344 Likes</span>
-            </div>
-            <span class="block ml-2 text-xs text-gray-600">5 minutes</span>
-          </div>
-
           <div class="pt-4 pb-1 pr-3">
             <div class="flex items-start">
               <textarea
