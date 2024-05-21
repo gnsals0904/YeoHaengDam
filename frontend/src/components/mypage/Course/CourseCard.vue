ec1a2876e0be2efcb61dd3f27affe4838f19a99e
gnsals0904 <gnsals0904@naver.com>
2024-05-22 00:22:07 +0900

    [feat] 저장한 여행지 전부 불러오기 구현

---
+++ frontend/src/components/mypage/Course/CourseCard.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  course: Object,
+});
+</script>
+
+<template>
+  <div class="grid grid-cols-1 gap-10">
+    <div class="bg-[#F0F8FF] rounded-lg p-4 shadow-lg mb-5">
+      <h3 class="text-lg font-bold mb-2">{{ course.title }}</h3>
+      <div class="text-gray-500 mb-4">
+        <p>
+          {{ course.description }}
+        </p>
+      </div>
+      <a
+        class="bg-[#00B8D4] hover:bg-[#0097A7] text-white font-bold py-2 px-4 rounded-lg transition-colors"
+      >
+        자세히 보기
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
